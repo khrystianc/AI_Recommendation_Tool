404863ba24276a1b488711763755cc58f6706b95
Khrystian Clark <khrystian.clark@gmail.com>
2025-02-06 10:07:05 -0800

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,9 @@
+import os
 
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/myDatabase'
+    GOOGLE_OAUTH_CLIENT_ID = os.environ.get('GOOGLE_OAUTH_CLIENT_ID')
+    GOOGLE_OAUTH_CLIENT_SECRET = os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET')
