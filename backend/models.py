5ce1bf548b70b2c8b32444634f0c4f9d0cab96c7
Khrystian Clark <khrystian.clark@gmail.com>
2025-02-06 09:59:28 -0800

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    metadata = db.Column(db.JSON)
+
+class UserPreference(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    item_id = db.Column(db.Integer)
+    rating = db.Column(db.Integer)
